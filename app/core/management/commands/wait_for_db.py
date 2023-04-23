ae4cf3a57df27a18714442fb37399cda7d5f9076
Maciej Jakubiec <jakubiec.mac@gmail.com>
2023-04-23 19:13:30 +0200

    Fixed linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,7 +11,8 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Django command to wait for database."""
 
-    def handle(self,*args,**options):
+
+    def handle(self, *args, **options):
         """Entrypoint for command"""
         self.stdout.write('Waiting for database...')
         db_up = False
@@ -22,5 +23,5 @@ class Command(BaseCommand):
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
