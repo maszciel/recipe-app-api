d37d17d35daea300f6effac9f3c58152f67d6eb1
Maciej Jakubiec <jakubiec.mac@gmail.com>
2023-04-23 19:17:24 +0200

    Fixed linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,9 +8,9 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for database."""
-
 
     def handle(self, *args, **options):
         """Entrypoint for command"""
